7db48900197405999530c03fb432b0dea7d665b9
JCeroOne <jceroone@gmail.com>
2025-09-28 00:05:03 -0600

    Avances en "players"

--- backend/routes/players.py
+++ backend/routes/players.py
@@ -18,12 +18,21 @@ def landing():
         "msg": "OK"
     }), code
 
-@player_routes.route("/{puuid}", methods=["GET"])
-def get_by_puuid():
+@player_routes.route("/<puuid>", methods=["GET"])
+def get_by_puuid(puuid):
+    key = os.getenv("RIOT_API_KEY")
+    req = requests.get(f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-puuid/{puuid}?api_key={key}")
+    if(req.status_code != 200):
+        code = 404
+        return jsonify({
+            "code": code,
+            "msg": "Account not found"
+        }), code
+    
     code = 200
     return jsonify({
         "code": code,
-        "msg": "OK"
+        "data": req.json()
     }), code
 
 @player_routes.route("/<user>/<tag>", methods=["GET"])
